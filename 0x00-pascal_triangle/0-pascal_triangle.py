34b8aa8d0c057ad336f2990f8980a7ccac2e8dc0
Ikenna <ikenna@kali.Ikenna-PC>
2023-03-25 19:08:08 +0100

    (fix pycodestyle) Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,15 +5,15 @@ Pascal triangle
 
 
 def pascal_triangle(n):
-  """
-  returns a list of lists of integers
-  representing the pascal's triangle of n
-  """
+    """
+    returns a list of lists of integers
+    representing the pascal's triangle of n
+    """
 
-  triagle = []
-  for i in range(n):
-    row = [1] * (i + 1)
-    for j in range(1, i):
-      row[j] = triagle[i - 1][j - 1] + triagle[i - 1][j]
-    triagle.append(row)
-  return triagle
+    triagle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triagle[i - 1][j - 1] + triagle[i - 1][j]
+        triagle.append(row)
+    return triagle
