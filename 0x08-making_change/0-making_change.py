d3bd4c38efb45be66f6699bd069197187ea450a8
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-21 23:19:23 +0100

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from typing import List
+
+
+def makeChange(coins, total):
+    """makechange function
+    :param coins: a list of the values of the coins in your possession
+    :param total: the given total
+    :return: 0 if total is 0 or less
+          :  -1 if total cannot be met
+          : fewest number of coins needed to meet total
+    """
+    if total < 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# Alx interview question
+
+## Making Change
